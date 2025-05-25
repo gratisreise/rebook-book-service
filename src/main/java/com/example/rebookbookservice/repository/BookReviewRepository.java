d3cbf1e5132bdc251ce7b492c0a66212869eae95
Jungho Kim <jeonghogim318@gmail.com>
2025-05-25 11:48:42 +0900

    fix: 설정파일 오류 해결

--- src/main/java/com/example/rebookbookservice/repository/BookReviewRepository.java
+++ src/main/java/com/example/rebookbookservice/repository/BookReviewRepository.java
@@ -1,9 +1,10 @@
 package com.example.rebookbookservice.repository;
 
+import com.example.rebookbookservice.model.entity.BookReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookReviewRepository extends JpaRepository<BookReviewRepository, Long> {
+public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
 
 }
